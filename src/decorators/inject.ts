fc7419fb73653539755734db659789f5b02a6656
shampoo6 <shampoo6@163.com>
2023-02-16 09:04:08 +0800

    1.0.1

--- src/decorators/inject.ts
+++ src/decorators/inject.ts
@@ -1,11 +1,11 @@
 import ObjectManager from '../core/ObjectManager';
-import {GlobalKey} from '../constants/GlobalKey';
+import { GlobalKey } from '../constants/GlobalKey';
 
 export default function inject(className: string) {
-    return (target: any, propName: string) => {
-        const key = `${target.constructor.name}.${propName}`;
-        global[GlobalKey.InjectMap].set(key, () => {
-            ObjectManager.getInstance().get(target.constructor.name)[propName] = ObjectManager.getInstance().get(className);
-        });
-    };
+  return (target: any, propName: string) => {
+    const key = `${target.constructor.name}.${propName}`;
+    global[GlobalKey.InjectMap].set(key, () => {
+      ObjectManager.getInstance().get(target.constructor.name)[propName] = ObjectManager.getInstance().get(className);
+    });
+  };
 }
